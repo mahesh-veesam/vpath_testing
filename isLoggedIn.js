4a05c95a4c1877fa034acf199932e55484ecde07
mahesh-veesam <veesammaheshmahee@gmail.com>
2025-10-01 04:02:12 +0530

    changed files for deployment ready

--- isLoggedIn.js
+++ isLoggedIn.js
@@ -1,4 +1,4 @@
-function isLoggedIn(req, res, next) {
-  if (req.user) return next();
-  res.status(401).json({ error: "User must be logged in" });
+export function isLoggedIn(req, res, next) {
+    if (req.user) return next();
+    res.status(401).json({ error: "You must be logged in" });
 }
