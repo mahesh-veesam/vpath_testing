3e1f4a33a304439f72af6c8b919efca0c7f5c337
mahesh-veesam <veesammaheshmahee@gmail.com>
2025-09-30 21:02:11 +0530

    changed files for deployment ready

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ app.use(express.json())
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:5000/auth/google/callback",
+  callbackURL: "https://vpath_testing.onrender.com/auth/google/callback",
   scope : ["profile","email"],
 },
 
